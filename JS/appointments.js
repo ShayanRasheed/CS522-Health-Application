114a92cbfe1243dd5a99f066b35ac2be23d4e124
Shayan Rasheed <shayanrasheed001@gmail.com>
2024-11-30 22:12:21 -0600

    added filtering for appts

--- JS/appointments.js
+++ JS/appointments.js
@@ -2,13 +2,6 @@ const listDiv = document.getElementById('first');
 const displayDiv = document.getElementById('second');
 
 let appointments;
-let apptList = [];
-let generalList = [];
-let dentalList = [];
-let visionList = [];
-let specialistList = [];
-let therapyList = [];
-let nutritionList = [];
 
 fetch('../siteData/appointments.json')
   .then(response => response.json()) // Parse the response as JSON
@@ -41,28 +34,47 @@ fetch('../siteData/appointments.json')
         apptDiv.append(purpose);
         apptDiv.id = "appt";
 
-        apptList.push(apptDiv);
-
-        if(value["appointmentType"] == "General Checkup") {
-            generalList.push(apptDiv);
-        }
-        else if(value["appointmentType"] == "Dental") {
-            dentalList.push(apptDiv);
-        }
-        else if(value["appointmentType"] == "Vision") {
-            visionList.push(apptDiv);
-        }
-        else if(value["appointmentType"] == "Specialist") {
-            specialistList.push(apptDiv);
-        }
-        else if(value["appointmentType"] == "Therapy") {
-            therapyList.push(apptDiv);
-        }
-        else if(value["appointmentType"] == "Nutrition") {
-            nutritionList.push(apptDiv);
-        }
-
         listDiv.append(apptDiv);
       }
     }
   })
+
+  const filter = document.getElementById("filter")
+  filter.addEventListener("change", filterList);
+
+  function filterList() {
+    const type = filter.value;
+    listDiv.replaceChildren();
+    listDiv.append(filter);
+
+    for (const key in appointments) {
+        if (appointments.hasOwnProperty(key)) {
+            const value = appointments[key];
+
+            if(type == "None" || type == value["appointmentType"]) {
+                const apptDiv = document.createElement('div');
+                apptDiv.id = "appointment";
+        
+                const title = document.createElement('p');
+                title.textContent = value["appointmentName"];
+        
+                const location = document.createElement('p');
+                location.textContent = value["location"];
+        
+                const dateTime = document.createElement('p');
+                dateTime.textContent = value["date"] + " @ " + value["time"];
+        
+                const purpose = document.createElement('p');
+                purpose.textContent = "Details: " + value["purpose"];
+        
+                apptDiv.append(title);
+                apptDiv.append(location);
+                apptDiv.append(dateTime);
+                apptDiv.append(purpose);
+                apptDiv.id = "appt";
+        
+                listDiv.append(apptDiv);
+            }
+        }
+    }
+  }
